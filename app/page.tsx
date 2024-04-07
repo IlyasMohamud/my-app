09fdb3cebd1414564b409e1cbc555a079829f4a1
Ilyas <ilyasmo_1999@hotmail.co.uk>
2024-04-07 13:07:41 +0200

    fixing error

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>
-        Hey! I'm a 24-year-old studying for my Master's in Informatics at UiB, having completed my Bachelor's in Data Technology at the same university. 
-        I have a real passion for machine learning and web development, and I'm all about using innovative technologies to create solutions that make a difference. 
-        On this webpage, you'll find me sharing my journey, experiences, and projects in these exciting fields. 
-        If you're into creating positive change through technology, let's connect and explore what we can achieve together!
+          "hello"
           </h1>
       </div>
     </main>
