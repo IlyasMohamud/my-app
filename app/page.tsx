f2e7d0a9ad7d592dae1e2b54ea36f258b2fd9b7f
Ilyas <ilyasmo_1999@hotmail.co.uk>
2024-04-07 13:03:34 +0200

    added an intro about myself

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>
-          hello
+        Hey! I'm a 24-year-old studying for my Master's in Informatics at UiB, having completed my Bachelor's in Data Technology at the same university. 
+        I have a real passion for machine learning and web development, and I'm all about using innovative technologies to create solutions that make a difference. 
+        On this webpage, you'll find me sharing my journey, experiences, and projects in these exciting fields. 
+        If you're into creating positive change through technology, let's connect and explore what we can achieve together!
           </h1>
       </div>
     </main>
