e4c7a075bf50e1269d5fbd94a2de679192ec9986
Ilyas <ilyasmo_1999@hotmail.co.uk>
2024-04-07 13:20:24 +0200

    trying to make blog

---
+++ app/blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog: React.FC = () => {
+  return (
+    <div>
+      <h2>Blog</h2>
+      {/* Blog content goes here */}
+    </div>
+  );
+};
+
+export default Blog;
