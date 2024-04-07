c496044c586ae69962e0bc8ae4fec3a0c8faf24e
Ilyas <ilyasmo_1999@hotmail.co.uk>
2024-04-07 13:22:33 +0200

    fixing bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
+import React from 'react';
 
-export default function Home() {
+const HomePage: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>
-          hello
-          </h1>
-      </div>
-    </main>
+    <div>
+      <section>
+        {/* Your picture and description */}
+        <h1>About Me</h1>
+        <p>Description here.</p>
+      </section>
+      {/* Other sections as needed */}
+    </div>
   );
-}
+};
+
+export default HomePage;
